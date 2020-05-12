9eeae73e031edeea29fb5d122ecd4c4171297659
Muhamar <amararrasyid@gmail.com>
2020-05-12 18:09:16 +0700

    Tugas 19 Javascript
    Mengumpulkan Tugas niomic

---
+++ index.js
@@ -0,0 +1,20 @@
+function repeat() {
+    var isi = "Semangat...";
+    console.log(isi.repeat(5));
+
+}
+
+
+function bintang() {
+    var bint = "*";
+    console.log(bint.repeat(5));
+    console.log(bint.repeat(25));
+    console.log(bint.repeat(50));
+    console.log(bint.repeat(60));
+    console.log(bint.repeat(50));
+    console.log(bint.repeat(25));
+    console.log(bint.repeat(5));
+}
+
+repeat()
+bintang()
